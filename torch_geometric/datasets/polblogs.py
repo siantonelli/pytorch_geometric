ee2ceddf7902099f02c1e455b5e59be8c0f5009a
santonelli7 <simone.antonelli97@gmail.com>
2023-06-16 11:24:30 +0200

    kwargs PolBlogs

--- torch_geometric/datasets/polblogs.py
+++ torch_geometric/datasets/polblogs.py
@@ -53,7 +53,7 @@ class PolBlogs(InMemoryDataset):
     url = 'https://netset.telecom-paris.fr/datasets/polblogs.tar.gz'
 
     def __init__(self, root: str, transform: Optional[Callable] = None,
-                 pre_transform: Optional[Callable] = None):
+                 pre_transform: Optional[Callable] = None, **kwargs):
         super().__init__(root, transform, pre_transform)
         self.data, self.slices = torch.load(self.processed_paths[0])
 
